4248b20e9a80aa198d90aef46c8b1f07b2756b3b
patriagani <patria.gani@gmail.com>
2018-09-25 19:29:12 +0700

    menambah exercises3

---
+++ week2/exercises3.js
@@ -0,0 +1,25 @@
+var nama = "patria";
+var peran = "Penyihir";
+
+if(nama === "" && peran === "") {
+  console.log("Nama harus diisi!");
+}
+
+else if(peran === "") {
+  console.log("Halo "+nama+ ", Pilih peranmu untuk memulai game!");
+}
+
+else if(nama !== "" && peran === "Ksatria") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Ksatria "+nama+", kamu dapat menyerang dengan senjatamu!");
+}
+
+else if(nama !== "" && peran === "Tabib") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Tabib "+nama+", kamu akan membantu temanmu yang terluka.");
+}
+
+else if(nama !== "" && peran === "Penyihir") {
+  console.log("Selamat datang di Dunia Proxytia, "+nama);
+  console.log("Halo Tabib "+nama+", ciptakan keajaiban yang membantu kemenanganmu!");
+}

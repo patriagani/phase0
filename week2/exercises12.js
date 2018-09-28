41161d84b3c79a7b9c2359547b2be537b52e4f14
patriagani <patria.gani@gmail.com>
2018-09-29 00:54:10 +0700

    revisi exercise12

--- week2/exercises12.js
+++ week2/exercises12.js
@@ -1,3 +1,8 @@
+/*Diberikan sebuah function konversiMenit(menit) yang menerima satu parameter
+ berupa angka yang merupakan ukuran waktu dalam menit. Function akan me-return
+ string waktu dalam format jam:menit berdasarkan menit tersebut. Contoh,
+ jika menit adalah 63, maka function akan me-return "1:03".*/
+
 function konversiMenit(menit) {
   var jam = Math.floor(menit/60)
   var sisa = menit%60

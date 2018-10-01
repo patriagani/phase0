6d7c460fa9f6a849dcf56a9d1ecf3b0884f845b3
patriagani <patria.gani@gmail.com>
2018-10-01 19:11:07 +0700

    revisi exercises4

--- week2/exercises4.js
+++ week2/exercises4.js
@@ -3,44 +3,48 @@ var bulan = 8;
 var tahun = 1945;
 var tanggallengkap
 
-switch(bulan) {
+if (tanggal < 32 && bulan < 13 && tahun < 2200 && tahun > 1900) {
+  switch(bulan) {
     case 1:
-        tanggallengkap = tanggal+" Januari "+tahun
-        break;
+    tanggallengkap = tanggal+" Januari "+tahun
+    break;
     case 2:
-        tanggallengkap = tanggal+" Februari "+tahun
-        break;
+    tanggallengkap = tanggal+" Februari "+tahun
+    break;
     case 3:
-        tanggallengkap = tanggal+" Maret "+tahun
-        break;
+    tanggallengkap = tanggal+" Maret "+tahun
+    break;
     case 4:
-        tanggallengkap = tanggal+" April "+tahun
-        break;
+    tanggallengkap = tanggal+" April "+tahun
+    break;
     case 5:
-        tanggallengkap = tanggal+" Mei "+tahun
-        break;
+    tanggallengkap = tanggal+" Mei "+tahun
+    break;
     case 6:
-        tanggallengkap = tanggal+" Juni "+tahun
-        break;
+    tanggallengkap = tanggal+" Juni "+tahun
+    break;
     case 7:
-        tanggallengkap = tanggal+" Juli "+tahun
-        break;
+    tanggallengkap = tanggal+" Juli "+tahun
+    break;
     case 8:
-        tanggallengkap = tanggal+" Agustus "+tahun
-        break;
+    tanggallengkap = tanggal+" Agustus "+tahun
+    break;
     case 9:
-        tanggallengkap = tanggal+" September "+tahun
-        break;
+    tanggallengkap = tanggal+" September "+tahun
+    break;
     case 10:
-        tanggallengkap = tanggal+" Oktober "+tahun
-        break;
+    tanggallengkap = tanggal+" Oktober "+tahun
+    break;
     case 11:
-        tanggallengkap = tanggal+" November "+tahun
-        break;
+    tanggallengkap = tanggal+" November "+tahun
+    break;
     case 12:
-        tanggallengkap = tanggal+" Desember "+tahun
-        break;
+    tanggallengkap = tanggal+" Desember "+tahun
+    break;
     default:
-        tanggallengkap = "Invalid month"
+    tanggallengkap = "Invalid month"
+  }
+} else {
+  tanggallengkap = "Invalid date, month or years";
 }
-  console.log(tanggallengkap);
+console.log(tanggallengkap);

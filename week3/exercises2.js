a80b96a42c85b945fd10cb35025e08f5fafa8cad
patriagani <patria.gani@gmail.com>
2018-10-02 13:13:03 +0700

    menambah exercises2

--- week3/exercises2.js
+++ week3/exercises2.js
@@ -1,3 +1,12 @@
+/*
+Buatlah sebuah fungsi dengan nama balikString. Fungsi ini akan menerima argumen sebuah string dan mengembalikan kebalikannya.
+Example
+
+//input "hello world!"
+//output
+"!dlrow olleh"
+*/
+
 function balikString(input) {
   var output = ""
   for (var i = input.length-1; i >= 0; i--) {

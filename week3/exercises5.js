83c5c4cf8df7f7a8b55f88c6de9c2af4b237cce7
patriagani <patria.gani@gmail.com>
2018-10-02 16:14:27 +0700

    revisi exercises5

--- week3/exercises5.js
+++ week3/exercises5.js
@@ -3,19 +3,19 @@ Diberikan sebuah function palindrome(kata) yang menerima satu parameter. Functio
 */
 
 function palindrome(kata) {
-  var katabaru = ""
+  var katabaru = "";
   for (var i = kata.length-1; i >= 0; i--) {
-    katabaru = katabaru + kata[i]
+    katabaru = katabaru + kata[i];
   }if (katabaru == kata) {
-    return true
+    return true;
   } else {
-    return false
+    return false;
   }
 }
 
 // TEST CASES
-console.log(palindrome('katak')); // true
-console.log(palindrome('blanket')); // false
-console.log(palindrome('civic')); // true
-console.log(palindrome('kasur rusak')); // true
-console.log(palindrome('mister')); // false
+console.log(palindrome("katak")); // true
+console.log(palindrome("blanket")); // false
+console.log(palindrome("civic")); // true
+console.log(palindrome("kasur rusak")); // true
+console.log(palindrome("mister")); // false
